93d7ecf04a6d62758fd4d0254200ac7cf364b0a5
Zachary Cui <zacharycui2009@gmail.com>
2022-06-13 21:57:38 +1200

    Modify colour scale to dynamically adjust to dataset

--- src/screens/WorldMap.tsx
+++ src/screens/WorldMap.tsx
@@ -4,6 +4,7 @@ import { observer } from "mobx-react-lite";
 import { scaleLinear } from "d3-scale";
 
 import { userStoreContext } from "../store/StoreContexts";
+import { Metrics } from "../components/DemographicSelector";
 
 let demographicData = require("../data/demographicData.json");
 let dieselData = require("../data/dieselPrice.json");
@@ -25,20 +26,33 @@ type geoObject = {
   type: String;
 };
 
-const colorScale = scaleLinear<string>()
-  .domain([0, 3])
-  .range(["#fcead2", "#f79716"]);
-
 const WorldMap: FunctionComponent = () => {
   const userStore = useContext(userStoreContext);
   const geoUrl =
     "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json";
 
+  const dataMap = new Map<Metrics, any>();
+  dataMap.set("ELECTRICITY", electrictyData);
+  dataMap.set("DIESEL", dieselData);
+  dataMap.set("GAS", gasData);
+
   const setColour = (geo: geoObject): string => {
-    return Object.keys(dieselData).find((key) => key == geo.properties.name)
-      ? colorScale(dieselData[geo.properties.name])
-      : "#F5F4F6";
+    if (userStore.selectedMetric) {
+      let data = dataMap.get(userStore.selectedMetric);
+      let arr = Object.values(data) as any;
+
+      const colorScale = scaleLinear<string>()
+        .domain([Math.min(...arr), Math.max(...arr)])
+        .range(["#fcead2", "#f79716"]);
+
+      return Object.keys(data).find((key) => key === geo.properties.name)
+        ? colorScale(data[geo.properties.name])
+        : "#F5F4F6";
+    }
+    return "#F5F4F6";
   };
+
+  console.log(userStore.selectedMetric);
 
   return (
     <>
@@ -52,7 +66,6 @@ const WorldMap: FunctionComponent = () => {
                 fill={setColour(geo)}
                 onMouseDown={() => {
                   userStore.selectCountry(geo.properties.name);
-                  console.log(colorScale(dieselData[geo.properties.name]));
                 }}
                 style={{
                   hover: {
